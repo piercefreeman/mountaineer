19c692607fad1def0058d367437a5075126b0c3b
Pierce Freeman <piercefreeman@gmail.com>
2024-01-26 23:56:04 -0800

    Add additional design notes and TODOs

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # filzl
+
+## Design TODOs:
+
+- Global methods for getting the link to another page (should validate these links are actually valid at build-time)
+- Conventions for naming controllers to automatically generate interface type names / global context state name that doens't conflict
+    - Use camel case? Either way we'll have to enforce no controller name conflicts on build.
+- How to use a preprocessor like tailwind for the bundling?
+- Values will passthrough EITHER if @passthrough is specified, or if an explicit fastapi.Response is provided. In this case the user is likely setting a cookie or other advanced metadata that we should include as-is.
+- For V1, we probably want to specify the routes in the controller using FastAPI's syntax. In the future we could also automatically derive the mapping from the view's hierarchy on disk. We just have to make sure that the controller->view mapping is unique. We should probably validate this anywhere when we do the initial build.
+
+NOTE:
+- We can also validate that all pages will be reachable by just loading the render function at runtime. This might have some unintended data mutation-side effects however so we should be careful / not do automatically.
+    - What's the safest way to retrieve the render function template then? Perhaps at the controller instance variable level instead? That wouldn't let us switch the view template depending on the request though... But do we really want to do that? That might invalidate our assumption that we can just update the render() by client-side reloading the state payload.
+    -> Decision: Force controller<->view mapping to be 1:1. We can enforce this right at the component level.
--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class ReturnModelMetaclass(ModelMetaclass):
                 raise
 
 class ReturnModel(BaseModel, metaclass=ReturnModelMetaclass):
-    template_path: str
+    pass
 
 class OtherObject(BaseModel):
     object_variable: str
@@ -62,6 +62,52 @@ class OtherObject(BaseModel):
 class MyResponse(ReturnModel):
     static_variable: str
     obj: OtherObject
+
+
+def generate_typescript_files():
+    # We need to generate one for the return of the render() function
+    # -> will model the server payload returned by useServer()
+    # This will be a type for the base model. This will be the interface that is used
+    # as the context provider for this particular controller.
+    # The global context provider will need to have a slot for every controller state, even if only
+    # one is used at a time.
+    # {
+    #   CONTROLLER_1_STATE?: Controller1State,
+    #   CONTROLLER_2_STATE?: Controller2State,
+    # }
+    # The useState provider will then only set the value for the current controller that's in scope
+    # on the page load.
+
+    # We also need for each action endpoint
+    # -> will model the fetch() payload returned by the different oneoff actions
+    # We might also need to generate subtypes for the @sideeffect in case the user
+    # only updates a subset of the fields
+    # The final payloads in general should look like:
+    # {
+    #    passthroughData,
+    #    sideEffectData: Either full state, or partial state. If partial state define inline.
+    # }
+
+    # There should be a common
+    # _request() class that will be used for all of these internally
+    # The implementations themselves will look more like:
+    # public static createUserPost({
+    #     requestBody,
+    # }: {
+    #     requestBody: RegisterSchema;
+    # }): CancelablePromise<User> {
+    #     return __request(OpenAPI, {
+    #         method: 'POST',
+    #         url: '/user/',
+    #         body: requestBody,
+    #         mediaType: 'application/json',
+    #         errors: {
+    #             422: `Validation Error`,
+    #         },
+    #     });
+    # }
+
+
 
 class MyInputPayload(BaseModel):
     input_variable: str
@@ -82,7 +128,35 @@ class MyInputPayload(BaseModel):
 class RouteController:
     pass
 
+
+def sideeffect(*args, **kwargs):
+    """
+    :param: If provided, will ONLY reload these fields. By default will reload all fields. Otherwise, why
+        specify a sideeffect at all?
+    """
+    def decorator_with_args(reload: Tuple[FieldClassDefinition, ...]):
+        print("SPECIFIC RELOAD", reload)
+        def wrapper(func: Callable):
+            @wraps(func)
+            def inner(*func_args, **func_kwargs):
+                return func(*func_args, **func_kwargs)
+            return inner
+        return wrapper
+
+    if args and callable(args[0]):
+        # It's used as @sideeffect without arguments
+        func = args[0]
+        return decorator_with_args(())(func)
+    else:
+        # It's used as @sideeffect(xyz=2) with arguments
+        return decorator_with_args(*args, **kwargs)
+
 class MyRouteController(RouteController):
+    # Unlike in next.js the post won't actually do anything, it's just to align conventions
+    # OR maybe this should actually determine URL paths? Easier to layout templates in terms
+    # of arbitrary paths than for them to be embedded in python itself.
+    template_path = "/testing/[post_id]/mytemplate.tsx"
+
     def render(
         self
         # Can inject dependencies here, like the request
@@ -95,71 +169,30 @@ class MyRouteController(RouteController):
             obj=OtherObject(
                 object_variable="world",
             ),
-            # Unlike in next.js the post won't actually do anything, it's just to align conventions
-            # OR maybe this should actually determine URL paths? Easier to layout templates in terms
-            # of arbitrary paths than for them to be embedded in python itself.
-            template_path="/testing/[post_id]/mytemplate.tsx",
         )
 
+    @sideeffect
     def my_action_with_sideeffect(self, payload: MyInputPayload):
-        #return sideeffect(
-        #   # This will throw by default in Pydantic, but maybe we allow it in our subclasses
-        #    reload=[MyResponse.static_variable, ...],
-        #)
-        return None
+        pass
 
     def my_action_without_sideeffect(self):
-        # Logic, return None
-        return None
+        pass
 
-    def other_value(self):
+    def other_action(self):
         # Maybe also provide the ability for side-effects to provide arbitrary data back that isn't
         # inscope of the render route itself?
         # What would this be? Credit card confirmation from Stripe, for instance?
+        # Maybe @passthrough to indicate it's not actually resetting the server state (unlike side-effect)
         pass
 
 if __name__ == "__main__":
     from pydantic import BaseModel
     from typing import Generic, TypeVar, Tuple, Type
 
-    # Define the types for WrapperElement
-    T = TypeVar('T')
-
     class MySpec(ReturnModel):
         testing: str
         testing2: int
         testing3: float  # Assuming you have a third field for demonstration
-
-    # Define a generic WrapperElement class
-    class WrapperElement(Generic[T]):
-        def __init__(self, *elements: T):
-            self.elements = elements
-
-        def __repr__(self):
-            return f"WrapperElement(elements={self.elements})"
-
-    def sideeffect(*args, **kwargs):
-        """
-        :param: If provided, will ONLY reload these fields. By default will reload all fields. Otherwise, why
-            specify a sideeffect at all?
-        """
-        def decorator_with_args(reload: Tuple[FieldClassDefinition, ...]):
-            print("SPECIFIC RELOAD", reload)
-            def wrapper(func: Callable):
-                @wraps(func)
-                def inner(*func_args, **func_kwargs):
-                    return func(*func_args, **func_kwargs)
-                return inner
-            return wrapper
-
-        if args and callable(args[0]):
-            # It's used as @sideeffect without arguments
-            func = args[0]
-            return decorator_with_args(())(func)
-        else:
-            # It's used as @sideeffect(xyz=2) with arguments
-            return decorator_with_args(*args, **kwargs)
-
 
     # Example usage
     #wrapper1 = WrapperElement(MySpec.testing, MySpec.testing2)
