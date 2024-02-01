cd890ab50c0a381978af9c9dd2eae0591bab0671
Pierce Freeman <piercefreeman@gmail.com>
2024-01-31 18:24:47 -0800

    Add test for render dependency resolution

--- filzl/__tests__/actions/test_sideeffect.py
+++ filzl/__tests__/actions/test_sideeffect.py
@@ -1,7 +1,13 @@
+from pathlib import Path
+
+import pytest
+from fastapi import Depends, Request
 from pydantic.main import BaseModel
+from starlette.datastructures import Headers
 
 from filzl.actions.fields import FunctionActionType, get_function_metadata
-from filzl.actions.sideeffect import sideeffect
+from filzl.actions.sideeffect import get_render_parameters, sideeffect
+from filzl.app import AppController
 from filzl.controller import ControllerBase
 from filzl.render import RenderBase
 
@@ -40,3 +46,69 @@ def test_markup_sideeffect():
     assert metadata.url is None
     assert metadata.return_model is None
     assert metadata.render_router is None
+
+
+@pytest.mark.asyncio
+async def test_get_render_parameters():
+    """
+    Given a controller, reproduce the logic of FastAPI to sniff the render()
+    function for dependencies and resolve them.
+
+    """
+    found_cookie = None
+
+    def grab_cookie_dependency(request: Request):
+        nonlocal found_cookie
+        found_cookie = request.cookies.get("test-cookie")
+        return found_cookie
+
+    class ExampleRenderModel(RenderBase):
+        value_a: str
+        value_b: str
+
+    class TestController(ControllerBase):
+        url: str = "/test/{query_id}/"
+
+        def render(
+            self,
+            query_id: int,
+            cookie_dependency: str = Depends(grab_cookie_dependency),
+        ) -> ExampleRenderModel:
+            return dict(
+                value_a="Hello",
+                value_b="World",
+            )
+
+    # We need to load this test controller to an actual application runtime
+    # or else we don't have the render() metadata added
+    app = AppController(Path())
+    controller = TestController()
+    app.register(controller)
+
+    fake_request = Request(
+        {
+            "type": "http",
+            "headers": Headers(
+                {
+                    "cookie": "test-cookie=cookie-value",
+                    # Its important the referer aligns with the controller url, since that is expected
+                    # to be the original view page that is calling this sub-function
+                    "referer": "http://example.com/test/5/",
+                }
+            ).raw,
+            "http_version": "1.1",
+            "scheme": "",
+            "client": "",
+            "server": "",
+            # The URL and method should both be different, to test whether we are able
+            # to map the request to the correct endpoint
+            "method": "POST",
+            "url": "http://localhost/related_action_endpoint",
+        }
+    )
+
+    async with get_render_parameters(controller, fake_request) as resolved_dependencies:
+        assert resolved_dependencies == {
+            "cookie_dependency": "cookie-value",
+            "query_id": 5,
+        }
