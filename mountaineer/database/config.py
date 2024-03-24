d441c1812b622331ef8e2117b1d9ef6ed215cc26
Pierce Freeman <piercefreeman@gmail.com>
2024-03-24 13:38:35 -0700

    Accept class-variables for database definitions

--- mountaineer/database/config.py
+++ mountaineer/database/config.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Any
 
 from pydantic import PostgresDsn, model_validator
+from pydantic_core import PydanticUndefinedType
 from pydantic_settings import BaseSettings
 
 
@@ -28,13 +29,24 @@ class DatabaseConfig(BaseSettings):
 
     @model_validator(mode="before")
     def build_db_connection(cls, values: Any) -> Any:
+        # Support users specifying some defaults via the pydantic
+        # model directly
+        cls_defaults = {
+            field_name: field.default
+            for field_name, field in cls.model_fields.items()
+            if field.default and not isinstance(field.default, PydanticUndefinedType)
+        }
+
+        all_values = {**cls_defaults, **values}
+
         if not values.get("SQLALCHEMY_DATABASE_URI"):
             values["SQLALCHEMY_DATABASE_URI"] = PostgresDsn.build(  # type: ignore
                 scheme="postgresql+asyncpg",
-                username=values["POSTGRES_USER"],
-                password=values["POSTGRES_PASSWORD"],
-                host=values["POSTGRES_HOST"],
-                port=int(values.get("POSTGRES_PORT", 5432)),
-                path=values["POSTGRES_DB"],
+                username=all_values["POSTGRES_USER"],
+                password=all_values["POSTGRES_PASSWORD"],
+                host=all_values["POSTGRES_HOST"],
+                port=int(all_values.get("POSTGRES_PORT", 5432)),
+                path=all_values["POSTGRES_DB"],
             )
+
         return values
