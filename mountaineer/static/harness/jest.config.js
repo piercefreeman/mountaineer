bffd901ccc4aa64d192881c24874b447e26c3ea7
Pierce Freeman <piercefreeman@gmail.com>
2025-01-15 10:28:41 -0800

    Include harness with static files for typehinting

---
+++ mountaineer/static/harness/jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.json'
+    }]
+  }
+};
