a187aeb4ba8b0b24340e4524424cbd385906356d
Pierce Freeman <piercefreeman@gmail.com>
2026-03-29 21:23:24 -0700

    Use mountaineer-di for introspection

--- mountaineer/dependencies/__init__.py
+++ mountaineer/dependencies/__init__.py
@@ -3,4 +3,5 @@ from .base import (
     DependenciesBase as DependenciesBase,
     get_function_dependencies as get_function_dependencies,
     isolate_dependency_only_function as isolate_dependency_only_function,
+    strip_depends_from_signature as strip_depends_from_signature,
 )
